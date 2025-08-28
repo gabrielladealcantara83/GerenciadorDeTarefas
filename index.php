ac82fb5d5d398dd9a36a7bccd2a35f599bc4b259
GABRIELLA CAMPOS DE ALCANTARA <gabriella.2924@aluno.pr.senac.br>
2025-08-28 10:56:44 -0300

    Criando tela de login, criando botao de logout com icone

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
+ 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,12 +12,17 @@
         integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
         crossorigin="anonymous">
 </head>
-
+ 
 <body>
     <div class="container">
         <nav class="navbar navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="index.php">Crud PHP </a>
+                <form action="login.php" method="GET" class="d-inline">
+                    <button type="submit" class="btn btn-secondary">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </button>
+                </form>
             </div>
         </nav>
         <main class="container p-4">
@@ -28,7 +33,7 @@
                         <form action="save.php" method="POST">
                             <div class="form-group">
                                 <input type="text" name="title" class="form-control"
-                                    placeholder="Task Title" autofocus>
+                                    placeholder="Task Title" autofocus required>
                             </div>
                             <div class="form-group">
                                 <textarea name="description" rows="2" class="form-control"
@@ -40,7 +45,7 @@
                     </div>
                 </div>
                 <!-- formulario -->
-
+ 
                 <!-- tabela de tarefas -->
                 <div class="col-md-8">
                     <table class="table table-bordered">
@@ -58,18 +63,18 @@
                             require_once 'conn.php';
                             $query = "SELECT id, title, description, created_at FROM crud_php";
                             $result = $conn->query($query);
-
+ 
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
                             ?>
                                     <tr>
-
+ 
                                         <td><?php echo $row['id']; ?></td>
                                         <td><?php echo $row['title']; ?></td>
-                                        <td><?php echo $row['description']; ?></td>
+                                        <td><?=substr($row['description'], 0, 20) . '...' ?></td>
                                         <td><?=date("d/m/Y" , strtotime($row['created_at'])); ?></td>
                                         <td><a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-secondary">
-
+ 
                                                 <i class="fas fa-marker"></i>
                                             </a>
                                             <a href="delete.php?id=<?= $row['id']; ?>" class="btn btn-danger">
@@ -84,7 +89,7 @@
                             }
                             $conn->close();
                             ?>
-
+ 
                         </tbody>
                     </table>
                 </div>
@@ -92,12 +97,13 @@
             </div>
         </main>
     </div>
-
+ 
     <!-- BOOTSTRAP 4 SCRIPT -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
+ 
 </body>
-
+ 
 </html>
+ 
